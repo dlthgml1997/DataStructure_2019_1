5c46ca7bc421ff844931a310d488df194e00e1c1
sohee <shfksthgml@naver.com>
2019-04-17 02:22:13 +0900

    <reverse_list.c> 리스트를 역순으로 만드는 연산

---
+++ LinkedList/reverse_list.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct ListNode {
+	element data;
+	struct ListNode *link;
+}ListNode;
+
+ListNode *insert_first(ListNode *head, element value)
+{
+	ListNode *p = (ListNode *)malloc(sizeof(p));
+	p->data = value;
+	p->link = head;
+	head = p;
+	return head;
+}
+
+void print_list(ListNode *head) {
+	for (ListNode *p = head; p != NULL; p = p->link)
+		printf("%d->",p->data);
+	printf("NULL \n");
+}
+
+ListNode * reverse(ListNode *head)
+{
+	//순회 포인터로 p,q,r을 사용
+	ListNode *p, *q, *r;
+
+	p = head;
+	q = NULL;
+		while(p != NULL) {
+			r = q;
+			q = p;
+			p = p->link;
+			q->link = r;
+		}
+	return q;
+}
+
+int main() {
+	ListNode * head1 = NULL;
+	ListNode * head2 = NULL;
+
+	head1 = insert_first(head1, 10);
+	head1 = insert_first(head1, 20);
+	head1 = insert_first(head1, 30); // 30 20 10
+	print_list(head1);
+
+	head2 = reverse(head1);
+	print_list(head2); //10 20 30
+	return 0;
+
+}
