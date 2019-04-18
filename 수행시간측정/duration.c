c7745a62f30caa5e89891b1a684b41f10d083477
sohee <shfksthgml@naver.com>
2019-04-18 21:58:29 +0900

    <duration.c> 수행시간 측정

---
+++ 수행시간측정/duration.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double slow_power(double x, int n)
+{
+
+}
+
+void main()
+{
+	clock_t start, finish;
+	double duration;
+	int i;
+	start = clock();
+	for (i = 0 ;i < 1000000; i++)
+		slow_power(1.125,1000);
+
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf(" duration(수행시간) = %lf \n", duration);
+
+
+}
