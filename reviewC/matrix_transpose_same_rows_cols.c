4613b3776cece946d3bd6edf09c5578353c5b216
sohee <shfksthgml@naver.com>
2019-04-20 00:59:59 +0900

    <matrix_transpose_same_rows_cols.c> 행과 열이 같을 때 전치

---
+++ reviewC/matrix_transpose_same_rows_cols.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
+//행렬 전치 함수
+void matrix_transpose(int A[ROWS][COLS], int B[ROWS][COLS]) //A = 전치할 행렬 ,B=전치된 행렬
+{
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLS; c++)
+			B[r][c] = A[c][r];
+	}
+}
+void matrix_print(int A[ROWS][COLS])
+{
+	printf("===========\n");
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLS; c++)
+			printf("%d", A[r][c]);
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int array[ROWS][COLS]={ {2,3,0},
+	{8,9,1},
+	{7,0,5} };
+
+	int array2[ROWS][COLS];
+	matrix_transpose(array, array2);
+	matrix_print(array);
+	matrix_print(array2);
+
+	return 0;
+
+}
